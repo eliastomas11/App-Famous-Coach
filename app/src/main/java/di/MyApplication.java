3ef64b4f456a007ca98e34c8ded11d587976ab8b
EliasDev <eliastomasguerra@gmail.com>
2023-09-01 13:37:23 -0300

    notification push implemented

--- app/src/main/java/di/MyApplication.java
+++ app/src/main/java/di/MyApplication.java
@@ -1,14 +1,36 @@
 package di;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.example.yourfamouscoach.R;
 
 public class MyApplication extends Application {
 
+    public static final String CHANNEL_ID = String.valueOf(R.string.quote_channel_id);
     public AppContainer appContainer;
 
     @Override
     public void onCreate() {
         super.onCreate();
         appContainer = new AppContainer(this);
+        createNotificationChannel();
     }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.quote_notification_channel_name);
+            String description = getString(R.string.quote_notification_channel_description);
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
